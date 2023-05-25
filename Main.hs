bfb0ac6e1b34f152d6156f4379a71eaaa1bc0c75
gladkykhse <gladkyks@nswi177.gitlab.mff.cuni.cz>
2023-05-26 01:23:31 +0200

    added command line parsing

--- Main.hs
+++ Main.hs
@@ -1,9 +1,24 @@
 import System.IO
-import System.Random
+import System.Environment
 import Primes
 import Utils
 import RSA
 
 
+run_with_args :: [String] -> IO()
+run_with_args l
+    | head l == "-gen-keys" =
+        create_keys
+    | head l == "-encrypt" ||  head l == "-decrypt" =
+        let [_, pk_file, file_to_encrypt, file_to_save] = l
+        in
+            encrypt_decrypt_file pk_file file_to_encrypt file_to_save
+    | otherwise =
+        error "Invalid argument. Possible options: -gen-keys, -encrypt, -decrypt"
+
+
 main :: IO()
-main = create_keys
+main =
+    do 
+        args <- getArgs
+        run_with_args args
--- RSA.hs
+++ RSA.hs
@@ -11,18 +11,22 @@ create_keys = do
         phi = (p - 1) * (q - 1) 
     e <- choose_e phi
     let d = mod_inv e phi
-    -- writeFile "public.key" $ show e
-    -- writeFile "private.key" $ show d
-    let encrypted = encrypt 11111 e n
-    putStrLn $ show $ encrypted
-    putStrLn $ show $ decrypt encrypted d n
+    writeFile "public.key" $ (show e ++ " " ++ show n)
+    writeFile "private.key" $ (show d ++ " " ++ show n)
 
 
-encrypt :: Integer -> Integer -> Integer -> Integer
-encrypt msg e n =
-    pow_mod msg e n
+encrypt_decrypt_file :: String -> String -> String -> IO()
+encrypt_decrypt_file key_file file file_to_save =
+    do
+        key <- readFile key_file
+        contents <- readFile file
+        let [key_str, n_str] = words key
+            key_int = read key_str :: Integer
+            n = read n_str :: Integer
+            int = read contents :: Integer
+        writeFile file_to_save $ (show $ encrypt_decrypt_integer int key_int n)
+        
 
-
-decrypt :: Integer -> Integer -> Integer -> Integer
-decrypt msg d n =
-    pow_mod msg d n
+encrypt_decrypt_integer :: Integer -> Integer -> Integer -> Integer
+encrypt_decrypt_integer msg key n =
+    pow_mod msg key n
